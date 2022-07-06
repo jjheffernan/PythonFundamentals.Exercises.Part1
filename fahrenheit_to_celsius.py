bc9b9de5c7ee6de7e361c31e83183602e74a0a23
jjheffernan <jjheffernan99@gmail.com>
2022-07-06 14:43:53 -0400

    fahrenheit to celsius complete. exercises part 1 complete.

---
+++ fahrenheit_to_celsius.py
@@ -0,0 +1,6 @@
+# test comment
+# made by jj on 7/6/22, HBD mom!
+fahrenheit = float(input("What is today's fahrenheit temp? "))
+celsius = (fahrenheit - 32) * 5/9
+
+print(fahrenheit, " degrees fahrenheit is equal to %.2f degrees celsius" % celsius)
